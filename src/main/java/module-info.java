08ce5be717015fe4d9b2d7d92dfbc8718f62f5d8
Osama_ammar <174951166+Osamaa3mmar@users.noreply.github.com>
2024-07-19 15:20:02 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sweetsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sweetsystem to javafx.fxml;
-    exports com.example.sweetsystem;
-}
