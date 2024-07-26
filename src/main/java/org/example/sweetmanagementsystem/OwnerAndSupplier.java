66f70fc26e63cfc9a86d70b0999b1ae6efa65ab6
Abdulfattah Almallak <abdalmallak@hotmail.com>
2024-07-26 23:14:57 +0300

    Product management

--- src/main/java/org/example/sweetmanagementsystem/OwnerAndSupplier.java
+++ src/main/java/org/example/sweetmanagementsystem/OwnerAndSupplier.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 public class OwnerAndSupplier {
     private static int idCounter = 0;
-    private int id;
+    private final int id;
     private String name;
     private String email;
     private String accountDetails;
-    private static HashMap<Integer, OwnerAndSupplier> map = new HashMap<>();
-    private List<Message> sentMessages;
-    private List<Message> readMessages;
-    private List<Message> unreadMessages;
+    private static final HashMap<Integer, OwnerAndSupplier> map = new HashMap<>();
+    private final List<Message> sentMessages;
+    private final List<Message> readMessages;
+    private final List<Message> unreadMessages;
+    private final List<Product> products;
+    private static final String INVALID_ID_MESSAGE = "ID is invalid";
 
     public OwnerAndSupplier(String name, String email, String accountDetails) {
         this.id = idCounter;
@@ -25,6 +27,7 @@ public class OwnerAndSupplier {
         sentMessages = new ArrayList<>();
         readMessages = new ArrayList<>();
         unreadMessages = new ArrayList<>();
+        products = new ArrayList<>();
     }
 
     public String getName() {
@@ -55,7 +58,7 @@ public class OwnerAndSupplier {
     public int sendMessage(String content, int receiverId) {
         Message message = new Message(this.id, receiverId, content);
         OwnerAndSupplier receiver = getOwnerSupplier(receiverId);
-        if(receiver == null) {
+        if (receiver == null) {
             return 1;
         }
         sentMessages.add(message);
@@ -63,14 +66,12 @@ public class OwnerAndSupplier {
         return 0;
     }
 
-
-
     public void receiveMessage(Message message) {
         unreadMessages.add(message);
     }
 
     public void readUnreadMessages() {
-        while(!unreadMessages.isEmpty()) {
+        while (!unreadMessages.isEmpty()) {
             Message message = unreadMessages.get(unreadMessages.size() - 1);
             System.out.println(message);
             unreadMessages.remove(unreadMessages.size() - 1);
@@ -78,8 +79,44 @@ public class OwnerAndSupplier {
         }
     }
 
-    static OwnerAndSupplier getOwnerSupplier(int id) {
+    //will be moved to User class
+    public static OwnerAndSupplier getOwnerSupplier(int id) {
         return map.getOrDefault(id, null);
     }
-}
 
+    public void increaseProductQuantity(int id, int quantity) {
+        Product product = Product.getProduct(id);
+        if (product != null && product.getOwnerID() == this.id) {
+            product.setQuantity(product.getQuantity() + quantity);
+        } else {
+            System.out.println(INVALID_ID_MESSAGE);
+        }
+    }
+
+    public void addNewProduct(String name, String description, double price, int quantity) {
+        Product product = new Product(name, description, price, quantity, this.id);
+        products.add(product);
+    }
+
+    public void updateProduct(int id, String name, String description, double price, int quantity) {
+        Product product = Product.getProduct(id);
+        if (product != null && product.getOwnerID() == this.id) {
+            product.setName(name);
+            product.setDescription(description);
+            product.setPrice(price);
+            product.setQuantity(quantity);
+        } else {
+            System.out.println(INVALID_ID_MESSAGE);
+        }
+    }
+
+    public void deleteProduct(int id) {
+        Product product = Product.getProduct(id);
+        if (product != null && product.getOwnerID() == this.id) {
+            products.remove(product);
+            Product.removeProduct(id);
+        } else {
+            System.out.println(INVALID_ID_MESSAGE);
+        }
+    }
+}
