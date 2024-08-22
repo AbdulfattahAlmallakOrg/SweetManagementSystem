8a4c5ae08fa2a7af6cc7ab722989d9f259142748
Abdulfattah Almallak <abdalmallak@hotmail.com>
2024-08-22 22:15:28 +0300

    coverage improvement 1.5

--- src/main/java/com/example/sweetsystem/clasess/OwnerAndSupplier.java
+++ src/main/java/com/example/sweetsystem/clasess/OwnerAndSupplier.java
@@ -42,10 +42,6 @@ public class OwnerAndSupplier extends User {
         return products;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products.addAll(products);
-    }
-
     public String sendMessage(String content, int receiverId) {
         Message message = new Message(getId(), receiverId, content);
         OwnerAndSupplier receiver = getOwnerSupplier(receiverId);
