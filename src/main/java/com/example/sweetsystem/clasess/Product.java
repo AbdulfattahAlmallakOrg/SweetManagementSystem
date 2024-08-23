850f4fa5a963f64ae9c9be44351f92bd28616a5d
Osamaa3mmar <osamaammarothman@gmail.com>
2024-08-23 09:32:22 +0300

    Issue improvment

--- src/main/java/com/example/sweetsystem/clasess/Product.java
+++ src/main/java/com/example/sweetsystem/clasess/Product.java
@@ -2,7 +2,6 @@ package com.example.sweetsystem.clasess;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Product {
     private static int idCounter = 0;
